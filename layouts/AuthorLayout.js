ba4061d5c0ab94a3a9bb580f9a32f76a0d7f6c01
TONY ANTONY <tonyantony300@gmail.com>
2023-05-04 11:19:43 +0530

    resume name updated, tailwind cersion rollback

--- layouts/AuthorLayout.js
+++ layouts/AuthorLayout.js
@@ -15,7 +15,7 @@ export default function AuthorLayout({ children, frontMatter }) {
             About
           </h1>
           <div>
-          <a href="/resume/resume.pdf" className="bg-gray-700 hover:bg-gray-800 text-white font-bold py-2 px-4 rounded" download>
+          <a href="/resume/tonyantony.pdf" className="bg-gray-700 hover:bg-gray-800 text-white font-bold py-2 px-4 rounded" download>
             Download Resume
           </a>
         </div>
